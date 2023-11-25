bacadc1f210d7dea881c5026fa01b5968f7659d3
Steven <digitalexile99@gmail.com>
2023-11-24 18:14:02 -0700

    init server actions

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,5 +1,27 @@
+import { create } from "@/app/actions/create-board";
+import { Button } from "@/components/ui/button";
+import { db } from "@/lib/db";
 import React from "react";
 
-export default function OrganizationIdPage() {
-  return <div>Organization page</div>;
+export default async function OrganizationIdPage() {
+  const boards = await db.board.findMany();
+  return (
+    <div className="flex flex-col space-y-4">
+      <form action={create}>
+        <input
+          type="text"
+          id="title"
+          name="title"
+          required
+          placeholder="Enter a board title"
+        />
+        <Button>Submit</Button>
+      </form>
+      <div className="space-y-2">
+        {boards.map((board) => (
+          <div key={board.id}>{board.title}</div>
+        ))}
+      </div>
+    </div>
+  );
 }
